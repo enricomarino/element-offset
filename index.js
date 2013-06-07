247e7ab21b99b4bee947a7897c53b34d1c0b6bd5
Enrico Marino <enrico.marino@email.com>
2013-06-07 12:08:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,37 +7,41 @@
  */
 
 /**
- * Expose `Element`
+ * Expose `offset`
  */
 
-module.exports = Element;
-
-/**
- * Element
- */
-
-function Element (el){
-  this.el = el;
-};
+module.exports = offset;
 
 /**
  * offset
- * Get left/top offset.
- *
- * @return {Element} this for chaining
- * @api public
  */
 
-Element.prototype.offset = function () {
-  var el = this.el;
-  var left = 0;
-  var top = 0;
-   
-  while (el) {
-    left += el.offsetLeft - el.scrollLeft + el.clientLeft;
-    y += el.offsetTop - el.scrollTop + el.clientTop;
-    el = el.offsetParent;
-  }
+function offset (element) {
 
-  return { left: left, top: top };
-};
+  /**
+   * offset
+   * Get left/top offset.
+   *
+   * @return {Element} this for chaining
+   * @api public
+   */
+  
+  element.prototype.offset = function () {
+    var el = this.el;
+    var left = 0;
+    var top = 0;
+     
+    while (el) {
+      left += el.offsetLeft - el.scrollLeft + el.clientLeft;
+      y += el.offsetTop - el.scrollTop + el.clientTop;
+      el = el.offsetParent;
+    }
+  
+    return { 
+      left: left, 
+      top: top 
+    };
+  };
+
+  return element;
+}
